bf140772d30ef130ee9d46b31777a96a36e99484
Divshrika <divshrika18@gmail.com>
2021-09-17 23:38:07 -0700

    SampleProgram

---
+++ src/org/hexa/Sample.java
@@ -0,0 +1,17 @@
+package org.hexa;
+
+public class Sample {
+
+	private void tc1() {
+		System.out.println("Welcome to Java");
+	}
+
+	private void tc2() {
+		System.out.println("Welcome to Selenium");
+	}
+
+	private void tc3() {
+		System.out.println("Welcome to Project");
+	}
+
+}
