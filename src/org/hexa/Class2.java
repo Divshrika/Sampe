4af527abcdb0ecb3ab02e20f9184a9e5e092b66d
Divshrika <divshrika18@gmail.com>
2021-09-18 20:06:04 -0700

    Added new methods

--- src/org/hexa/Class2.java
+++ src/org/hexa/Class2.java
@@ -2,4 +2,17 @@ package org.hexa;
 
 public class Class2 {
 
+	private void india() {
+		System.out.println("India");
+	}
+
+	private void australia() {
+		System.out.println("America");
+
+	}
+
+	private void america() {
+		System.out.println("America");
+
+	}
 }
